6c8330c3976f679ad77b0565a51ce04302888257
Angela-coello <angeladarko32@gmail.com>
2023-09-11 10:17:08 +0200

    initial commit

---
+++ src/main/java/com/example/acc_house/AccHouseApplication.java
@@ -0,0 +1,13 @@
+package com.example.acc_house;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/acc_house/AccHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.acc_house;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
